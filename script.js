9caecd7a598185fe56bafb123530b63164dc9855
ulle <ukkonenu@arcada.fi>
2022-11-07 15:50:12 +0200

    poop

---
+++ script.js
@@ -0,0 +1,16 @@
+function personalTrainer() {
+    let minuter = Number(prompt("Hur många minuter har du tränat idag?"));
+
+    if (minuter <= 10 && minuter != 0) {
+        document.querySelector("#output").innerText ="Bättre kan du!";
+    }
+    else if (minuter >= 120){
+        document.querySelector("#output").innerText ="Det var hurtigt av dig men kom ihåg att chilla lite emellanåt!";
+    }
+    else if (minuter == 0){
+        document.querySelector("#output").innerText ="Touch some grass";
+    }
+    else {
+        document.querySelector("#output").innerText ="Bra jobbat!";
+    }
+  }
